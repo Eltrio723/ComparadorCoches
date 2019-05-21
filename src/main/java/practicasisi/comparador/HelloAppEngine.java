fe8c8f8104a1157b47448ae981ecfa0a82ab8f56
alimando <andresillopro@gmail.com>
2019-05-21 17:53:08 +0200

    Página HTML y sacado de datos de la página
    El apartado del precio recoge a veces mas datos de la cuenta

--- src/main/java/practicasisi/comparador/HelloAppEngine.java
+++ src/main/java/practicasisi/comparador/HelloAppEngine.java
@@ -14,7 +14,12 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloAppEngine extends HttpServlet {
 
 	
-  @Override
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+@Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException {
 
